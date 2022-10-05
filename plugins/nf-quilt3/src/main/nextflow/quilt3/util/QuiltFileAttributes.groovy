2813c27312ace38ec41b45d4201d52f319c426b0
Ernest Prabhakar <ernest@quiltdata.io>
2022-10-05 14:35:10 -0700

    readAttributes

---
+++ plugins/nf-quilt3/src/main/nextflow/quilt3/util/QuiltFileAttributes.groovy
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2022, Quilt Data Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nextflow.quilt3.util
+
+import static java.lang.String.format;
+import java.nio.file.attribute.BasicFileAttributeView
+import java.nio.file.attribute.BasicFileAttributes
+import java.nio.file.attribute.FileTime
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+
+/**
+ * Implements {@link BasicFileAttributes} for Quilt storage blob
+ *
+ * @author Ernest Prabhakar <ernest@quiltdata.io>
+ */
+
+ @Slf4j
+ @CompileStatic
+ class QuiltFileAttributes implements BasicFileAttributes {
+     private final FileTime lastModifiedTime;
+ 	private final long size;
+ 	private final boolean directory;
+ 	private final boolean regularFile;
+ 	private final String key;
+
+ 	public QuiltFileAttributes(String key, FileTime lastModifiedTime, long size,
+ 			boolean isDirectory, boolean isRegularFile) {
+ 		this.key = key;
+ 		this.lastModifiedTime = lastModifiedTime;
+ 		this.size = size;
+ 		directory = isDirectory;
+ 		regularFile = isRegularFile;
+ 	}
+
+ 	@Override
+ 	public FileTime lastModifiedTime() {
+ 		return lastModifiedTime;
+ 	}
+
+ 	@Override
+ 	public FileTime lastAccessTime() {
+ 		return lastModifiedTime;
+ 	}
+
+ 	@Override
+ 	public FileTime creationTime() {
+ 		return lastModifiedTime;
+ 	}
+
+ 	@Override
+ 	public boolean isRegularFile() {
+ 		return regularFile;
+ 	}
+
+ 	@Override
+ 	public boolean isDirectory() {
+ 		return directory;
+ 	}
+
+ 	@Override
+ 	public boolean isSymbolicLink() {
+ 		return false;
+ 	}
+
+ 	@Override
+ 	public boolean isOther() {
+ 		return false;
+ 	}
+
+ 	@Override
+ 	public long size() {
+ 		return size;
+ 	}
+
+ 	@Override
+ 	public Object fileKey() {
+ 		return key;
+ 	}
+
+ 	@Override
+ 	public String toString() {
+ 		return format(
+ 				"[%s: lastModified=%s, size=%s, isDirectory=%s, isRegularFile=%s]",
+ 				key, lastModifiedTime, size, directory, regularFile);
+ 	}
+ }
--- plugins/nf-quilt3/src/main/nextflow/quilt3/util/QuiltFileSystemProvider.groovy
+++ plugins/nf-quilt3/src/main/nextflow/quilt3/util/QuiltFileSystemProvider.groovy
@@ -37,6 +37,7 @@ import java.nio.file.attribute.BasicFileAttributeView
 import java.nio.file.attribute.BasicFileAttributes
 import java.nio.file.attribute.FileAttribute
 import java.nio.file.attribute.FileAttributeView
+import java.nio.file.attribute.FileTime
 import java.nio.file.spi.FileSystemProvider
 
 import groovy.transform.CompileStatic
@@ -366,12 +367,20 @@ class QuiltFileSystemProvider extends FileSystemProvider {
 
     @Override
     def <A extends BasicFileAttributes> A readAttributes(Path path, Class<A> type, LinkOption... options) throws IOException {
-        log.info "Ignoring call to BasicFileAttributes `readAttributes`: ${path}"
+        if( type == BasicFileAttributes || type == QuiltFileAttributes ) {
+            def qPath = asQuiltPath(path)
+            def key = qPath.toString()
+            def lastModifiedTime = FileTime.fromMillis(1_000_000_000_000)
+            def size = 1000
+
+            return (A) new QuiltFileAttributes(key, lastModifiedTime, size, true, false);
+        }
+        throw new UnsupportedOperationException("Not a valid Quilt Storage file attribute type: $type")
     }
 
     @Override
     Map<String, Object> readAttributes(Path path, String attributes, LinkOption... options) throws IOException {
-        log.info "Ignoring call to Map `readAttributes`: ${path}"
+        throw new UnsupportedOperationException("Operation Map 'readAttributes' is not supported by QuiltFileSystem")
     }
 
     @Override
