8cb6923325938dbb761c89d16db9933b70e5b5c5
Ernest Prabhakar <ernest@quiltdata.io>
2022-10-05 14:34:46 -0700

    cleanup headers

--- plugins/nf-quilt3/src/main/nextflow/quilt3/QuiltOpts.groovy
+++ plugins/nf-quilt3/src/main/nextflow/quilt3/QuiltOpts.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2022, Seqera Labs
+ * Copyright 2022, Quilt Data Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package nextflow.quilt3
@@ -65,7 +64,7 @@ class QuiltOpts {
         //   https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
         //
         // If not using the default, the profile needs to be set in the nextflow config file.
-        
+
         final config = fromSession(session)
         return config
     }
--- plugins/nf-quilt3/src/main/nextflow/quilt3/util/QuiltPathSerializer.groovy
+++ plugins/nf-quilt3/src/main/nextflow/quilt3/util/QuiltPathSerializer.groovy
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-
 package nextflow.quilt3.util
 
 import groovy.transform.CompileStatic
